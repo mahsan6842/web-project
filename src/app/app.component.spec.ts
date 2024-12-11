19d496bc77bc89b535fbf6d95e1f8703f2244a21
mahsan6842 <mahsan6842@gmail.com>
2024-12-11 09:53:11 -0800

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'untitled5' title`, () => {
+  it(`should have the 'untitled7' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('untitled5');
+    expect(app.title).toEqual('untitled7');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, untitled5');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, untitled7');
   });
 });
