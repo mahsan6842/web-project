5c5d4536dbeee09500d25f5b2fed2d44875b5047
mahsan6842 <mahsan6842@gmail.com>
2024-12-11 23:11:20 -0800

    Add files via upload

---
+++ src/app/deleteemployee/deleteemployee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deleteemployee',
+  imports: [],
+  templateUrl: './deleteemployee.component.html',
+  styleUrl: './deleteemployee.component.css'
+})
+export class DeleteemployeeComponent {
+
+}
---
+++ src/app/editemployee/editemployee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editemployee',
+  imports: [],
+  templateUrl: './editemployee.component.html',
+  styleUrl: './editemployee.component.css'
+})
+export class EditemployeeComponent {
+
+}
