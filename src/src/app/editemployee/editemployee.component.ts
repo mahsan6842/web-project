27b68e9a1647ac947a13fe816e396af53a044ccb
Faizybro <faizanmajeed78600@gmail.com>
2024-12-19 17:07:02 +0500

    Add files via upload

--- src/src/app/editemployee/editemployee.component.ts
+++ src/src/app/editemployee/editemployee.component.ts
@@ -1,12 +1,70 @@
 import { Component } from '@angular/core';
+import {LoginService} from '../login.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-editemployee',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf,
+    ReactiveFormsModule
+  ],
   templateUrl: './editemployee.component.html',
   standalone: true,
   styleUrl: './editemployee.component.css'
 })
 export class EditemployeeComponent {
+  emp = {
+    id:'',
+    name: '',
+    email:'',
+    position: ''
+  }
+  public blog !: any;
+  constructor(private loginService: LoginService,private route: ActivatedRoute ) {}
+  ngOnInit(): void {
 
+    // Fetch blog data and populate the edit object
+    this.loginService.getid(this.route.snapshot.paramMap.get('id')).subscribe(
+      (data) => {
+        this.blog = data;
+        this.emp = {
+          id: this.blog.id,
+          name: this.blog.name,
+          email: this.blog.email,
+          position: this.blog.position,
+        };
+      },
+      (err) => {
+        console.log("Retrieve failed", err);
+      }
+    );
+  }
+
+
+
+
+  isSubmitted = false;
+
+  onSubmit(form: any): void {
+    if (form.valid) {
+      console.log('Post Data:', this.emp);
+      this.isSubmitted = true;
+      this.loginService.updatedata(this.emp.id,this.emp).subscribe(
+        (data) => {
+          console.log("data sent success");
+        },
+        (error) => {
+          console.log("data sent failed");
+        });{ }
+
+      // Hide success message after 3 seconds
+      setTimeout(() => {
+        this.isSubmitted = false;
+        form.resetForm();
+      }, 3000);
+    }
+  }
 }
--- src/src/app/viewemployee/viewemployee.component.ts
+++ src/src/app/viewemployee/viewemployee.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login.service';
+import {NgForOf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 @Component({
   selector: 'app-viewemployee',
-  imports: [],
+  imports: [NgForOf, RouterLink],
   templateUrl: './viewemployee.component.html',
   standalone: true,
   styleUrl: './viewemployee.component.css'
 })
-export class ViewemployeeComponent {
+export class ViewemployeeComponent implements OnInit {
+  employees: any[] = [];
 
+  constructor(private loginService: LoginService) {}
+
+  ngOnInit(): void {
+    this.fetchEmployees();
+  }
+
+  fetchEmployees(): void {
+    this.loginService.getemployees().subscribe(
+      (data) => {
+        this.employees = data;
+        console.log("Employees fetched successfully:", this.employees);
+      },
+      (error) => {
+        console.log("Error fetching employees:", error);
+      }
+    );
+  }
+
+  deleteEmployee(id: any): void {
+    this.loginService.deleteEmployee(id).subscribe(
+      () => {
+        console.log("Employee deleted successfully");
+        this.employees = this.employees.filter((employee) => employee.id !== id);
+      },
+      (error) => {
+        console.log("Error deleting employee:", error);
+      }
+    );
+  }
 }
